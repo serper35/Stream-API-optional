d9185569fd38252f17ee0e6d54bd8ff5042e543e
serper35 <serper.wolf@gmail.com>
2023-11-01 21:23:15 +0300

    test

---
+++ Employes/src/main/java/pro/sky/Employes/EmployesApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.Employes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployesApplication.class, args);
+	}
+
+}
